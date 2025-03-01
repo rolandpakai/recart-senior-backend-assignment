8daec2de63693a65920edd5793d56373988d9341
roland.pakai <roland.pakai@lightware.com>
2025-03-01 14:44:05 +0100

    feat: connect to the db before starting the server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 /* eslint-disable no-console */
 import { Server } from 'http';
 import server from './server';
+import { connectToMongoDbWithRetries } from "./db";
 import { DEFAULT_PORT } from './defaults';
 
 export async function startServer(server: Server, PORT: number) {
+  try {
+    await connectToMongoDbWithRetries();
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.log("Failed to connect to MongoDB", e);
+  }
   try {
     server.listen(PORT, () => {
       console.log(`ReCart Service is running on http://localhost:${PORT}`);
